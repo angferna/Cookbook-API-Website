c4a1e680c82252f3738311b17087c5fb090e45ef
Madi Fawcett <madi.fawcett@du.edu>
2022-01-25 01:39:47 +0000

    Completed COMP3704 lab 05

--- server/api/users/index.js
+++ server/api/users/index.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import * as users from './users';
 
+// Declare an Express.js Router instance
 // let router = express.Router();
 let router = express.Router();
 
-router.get('./', users.listContents);
-router.get('./:id', users.findOne);
+// GET Methods
+router.get('/', users.listContents);
+router.get('/:id', users.findOne);
+
+// POST Methods
 router.post('/', users.createUser);
 
+// PUT Methods
+router.put('/:id', users.updateUser);
+
+// DELETE Methods
+router.delete('/:id', users.removeUser);
+
+// Export the Express.js Router to the routes.js
 export {router};
